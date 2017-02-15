e6052f537b419a72da4b6179c043a7bf36e2f76b
yamamotomakiko <yamamoto-makiko@av.avex.co.jp>
2017-02-15 12:28:18 +0000

    実習01

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  # deviseのコントローラを実行する時は「configure_permitted_parameters」メソッドを実行する
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    # 新規作成にname、thumbnail、agreementパラメータを含める
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :thumbnail, :agreement])
+  end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+         
+  # サムネイル画像にCarrierWaveで作成したUserThumbnailUploaderを使用
+  mount_uploader :thumbnail, UserThumbnailUploader
+  
+  validates :name, presence: true
+  validates :agreement, presence: true, acceptance: {accept: true}
+end
